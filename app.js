27a6dcd3652675da969e345f87adaf34777ad671
Kacper Witkowski <witkowskik46@gmail.com>
2023-02-17 08:35:47 +0100

    fix email address

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use((err, req, res, next) => {
 app.use(express.static("frontend/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  
 });
 
 app.listen(PORT);
